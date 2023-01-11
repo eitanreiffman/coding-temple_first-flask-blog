4af513ecef405dd8afb03d92c2ce9bd154c88e4f
Eitan Reiffman <eitanreiffman@gmail.com>
2023-01-11 13:44:08 -0500

    Updated config and SQLAlchemy routes

--- app/__init__.py
+++ app/__init__.py
@@ -1,7 +1,16 @@
+# Import Flask class from flask module - will be main object
 from flask import Flask
+# Import SQL Alchemy and Migrate from their modules
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+# Import the Config class from the config module - will have all the of the app's applications
+from config import Config
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = 'you-will-never-guess'
+app.config.from_object(Config)
+
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
 
 # import all of the routes from the routes file in the current folder
 from . import routes
